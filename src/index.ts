12d7a07e30304645a45f54c6c9627485a26532fa
thisdoraemon <ezrantn.dev@gmail.com>
2024-03-07 18:57:31 +0700

    built main function

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes";
+import tweetRoutes from "./routes/tweetRoutes";
+
+const app = express();
+app.use(express.json());
+app.use("/user", userRoutes);
+app.use("/tweet", tweetRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Hello World. Updated");
+});
+
+app.listen(3000, () => {
+    console.log("Server Ready at https://localhost:3000");
+});
