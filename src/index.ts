3cf28589bad91fce7851ad7ae62eaa6c361359fd
thisdoraemon <ezrantn.dev@gmail.com>
2024-03-08 20:21:33 +0700

    add api-spec endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import tweetRoutes from "./routes/tweetRoutes";
 import authRoutes from "./routes/authRoutes";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
+const swaggerSpec = YAML.load('api-spec.yaml');
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 app.use(express.json());
-app.use("/user", userRoutes);
-app.use("/tweet", tweetRoutes);
-app.use("/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/tweets", tweetRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
     res.send("Hello World. Updated");
