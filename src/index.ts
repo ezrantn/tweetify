262a13328145038d7e69b45d5318ba3e6f25411b
thisdoraemon <ezrantn.dev@gmail.com>
2024-03-11 22:30:55 +0700

    change api spec to json

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,17 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import tweetRoutes from "./routes/tweetRoutes";
 import authRoutes from "./routes/authRoutes";
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
 import { authMiddleware } from "./middleware/authMiddleware";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../api-spec.json";
 
 const app = express();
-const swaggerSpec = YAML.load('api-spec.yaml');
 
 app.use(express.json());
 app.use("/api/v1/users", authMiddleware, userRoutes);
 app.use("/api/v1/tweets", authMiddleware, tweetRoutes);
 app.use("/api/v1/auth", authRoutes);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
     res.send("Hello World. Updated");
