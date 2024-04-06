1149efe782264817bbf92c3dd452e71e2bb5099c
thisdoraemon <ezrantn.dev@gmail.com>
2024-04-06 21:49:59 +0700

    add routes for tweet and auth

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express from "express";
-import authRoutes from "./route/auth-routes";
 import { authMiddleware } from "./middleware/auth-middleware";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../api-spec.json";
 import UserController  from "./controller/user-controller";
 import TweetController from "./controller/tweet-controller";
+import { AuthController } from "./controller/auth-controller";
 
 const app = express();
 const apiPrefix = "/api/v1";
 
 app.use(express.json());
-
 
 // User Routes
 app.post(`${apiPrefix}/users`, UserController.createUserController);
@@ -19,9 +18,18 @@ app.get(`${apiPrefix}/users/:id`, authMiddleware, UserController.getUserByIDCont
 app.put(`${apiPrefix}/users/:id`, authMiddleware, UserController.updateUserController);
 app.delete(`${apiPrefix}/users/:id`, authMiddleware, UserController.deleteUserController);
 
+// Tweets Routes
+app.post(`${apiPrefix}/tweets`, authMiddleware, TweetController.createTweetController);
+app.get(`${apiPrefix}/tweets`, authMiddleware, TweetController.getAllTweetsController);
+app.get(`${apiPrefix}/tweets/:id`, authMiddleware, TweetController.getTweetByIdController);
+app.put(`${apiPrefix}/tweets/:id`, authMiddleware, TweetController.updateTweetController);
+app.delete(`${apiPrefix}/tweets/:id`, authMiddleware, TweetController.deleteTweetController);
 
-app.use(`${apiPrefix}/tweets`, authMiddleware, TweetController.createTweetController);
-app.use(`${apiPrefix}/auth`, authRoutes);
+// Auth Routes
+app.post(`${apiPrefix}/login`, AuthController.login);
+app.post(`${apiPrefix}/auth`, AuthController.authenticate);
+
+// Swagger Route
 app.use(`${apiPrefix}/docs`, swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
