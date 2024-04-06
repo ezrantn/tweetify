9f853eed2e69a9a74ce7c598d19f64c90e7c6416
thisdoraemon <ezrantn.dev@gmail.com>
2024-04-06 21:52:25 +0700

    add soft delete in delete user service

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -5,12 +5,21 @@ import { updateUserSchema, userSchema } from "../validation/user-schema";
 import { ResponseError } from "../error/error";
 import { User } from "../types/user-types";
 import { Tweet } from "../types/tweet-types";
+import { logger } from "../application/logger";
+
+/**
+ * Creates a new user.
+ * @param {User} userData - User data to be created.
+ * @returns {Promise<User>} The created user.
+ * @throws {ResponseError} If validation fails or a server error occurs.
+ */
 
 const createUser = async (userData: User): Promise<User> => {
   try {
     const { email, name, username } = validate(userSchema, userData);
 
     if (!email || !name || !username) {
+      logger.error("Missing fields when creating an account");
       throw new ResponseError(400, "Bad Request");
     }
 
@@ -28,10 +37,11 @@ const createUser = async (userData: User): Promise<User> => {
       error instanceof Prisma.PrismaClientKnownRequestError &&
       error.code === "P2002"
     ) {
+      logger.error("Prisma Error:", error);
       throw new ResponseError(400, "Username already exist");
     }
-
-    throw error;
+    logger.error("Failed to create user:", error);
+    throw new ResponseError(500, "Internal Server Error");
   }
 };
 
@@ -45,13 +55,14 @@ const getAllUsers = async (): Promise<User[]> => {
 
     return allUser;
   } catch (error) {
-    throw error;
+    logger.error("Failed to fetch all users:", error);
+    throw new ResponseError(500, "Failed to fetch users");
   }
 };
 
 const getUserByID = async (
   id: string
-): Promise<{ user: User | null; tweet: Tweet[] | null }> => {
+): Promise<{ user: User | null }> => {
   try {
     const user = await prismaClient.user.findUnique({
       where: { id: Number(id) },
@@ -59,12 +70,13 @@ const getUserByID = async (
     });
 
     if (!user) {
-      return { user: null, tweet: null };
+      logger.error("User not Found for ID: ", id);
+      throw new ResponseError(404, "User not found!");
     }
 
-    return { user, tweet: user.tweets };
+    return { user };
   } catch (error) {
-    console.error("Error fetching user:", error);
+    logger.error("Error fetching user by ID:", error);
     throw new ResponseError(
       500,
       "An error occurred while fetching the user. Please try again later."
@@ -88,20 +100,41 @@ const updateUser = async (id: string, newUser: User): Promise<User> => {
         email,
       },
     });
+
+    if (!result) {
+      logger.error("User not found for ID:", id);
+      throw new ResponseError(404, "User not found");
+    }
     return result;
   } catch (error) {
-    throw error;
+    logger.error(`Failed to update user with ID: ${id}`, error);
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      throw new ResponseError(400, "Invalid data provided for update");
+    } else {
+      throw new ResponseError(500, "Internal Server Error");
+    }
   }
 };
 
-const deleteUser = async (id: string): Promise<User> => {
+const deleteUser = async (id: string): Promise<void> => {
   try {
-    const deletedUser = await prismaClient.user.delete({
-      where: { id: Number(id) },
+    const deletedUser = await prismaClient.user.update({
+      where: { id: Number(id)},
+      data: {
+        deletedAt: new Date()
+      }
     });
-    return deletedUser;
+
+    if (!deletedUser) {
+      logger.error("User not found for ID:", id);
+      throw new ResponseError(404, "User not found");
+    }
   } catch (error) {
-    throw error;
+    logger.error(`Failed to delete user with ID: ${id}`, error);
+    throw new ResponseError(500, "Internal Server Error");
   }
 };
 
