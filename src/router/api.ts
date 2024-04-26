37a31e8bd11587a70509b7d1b567a836595c1dbe
thisdoraemon <ezrantn.dev@gmail.com>
2024-04-26 12:14:33 +0700

    refactoring

--- src/router/api.ts
+++ src/router/api.ts
@@ -12,6 +12,7 @@ privateApi.get("/api/v1/users", userController.getAllUsersController);
 privateApi.get("/api/v1/users/:id", userController.getUserByIDController);
 privateApi.put("/api/v1/users/:id", userController.updateUserController);
 privateApi.delete("/api/v1/users/:id", userController.deleteUserController);
+privateApi.post("/api/v1/upload/:id", userController.uploadAvatarController);
 
 // Tweet Routes
 privateApi.post("/api/v1/tweets", tweetController.createTweetController);
