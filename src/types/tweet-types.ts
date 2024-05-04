cbb85ed8007cb2f78458383a9b1e66dd1dd9814c
thisdoraemon <ezrantn.dev@gmail.com>
2024-05-04 16:57:48 +0700

    change from interface to class for modularity

--- src/types/tweet-types.ts
+++ src/types/tweet-types.ts
@@ -1,8 +1,17 @@
-export interface Tweet {
+export class Tweet {
   id: number;
   createdAt: Date;
   updatedAt: Date;
   content: string;
   image: string | null;
   impression: number;
+
+  constructor(id: number, createdAt: Date, updatedAt: Date, content: string, image: string | null, impression: number) {
+    this.id = id;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.content = content;
+    this.image = image;
+    this.impression = impression;
+  }
 }
--- src/types/user-types.ts
+++ src/types/user-types.ts
@@ -1,4 +1,4 @@
-export interface User {
+export class User {
   id: number;
   createdAt: Date;
   updatedAt: Date;
@@ -8,4 +8,16 @@ export interface User {
   bio: string | null;
   image: string | null;
   isVerified: boolean;
+
+  constructor(id: number, createdAt: Date, updatedAt: Date, email: string, name: string | null, username: string | null, bio: string | null, image: string | null, isVerified: boolean) {
+    this.id = id;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.email = email;
+    this.name = name;
+    this.username = username;
+    this.bio = bio;
+    this.image = image;
+    this.isVerified = isVerified;
+  }
 }
