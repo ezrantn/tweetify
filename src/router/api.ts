ad7aa70f8797d06c02d398c644ea04f533bdfe7f
thisdoraemon <ezrantn.dev@gmail.com>
2024-05-04 12:51:48 +0700

    added multer upload param

--- src/router/api.ts
+++ src/router/api.ts
@@ -2,9 +2,13 @@ import express from "express";
 import userController from "../controller/user-controller";
 import authMiddleware from "../middleware/auth-middleware";
 import tweetController from "../controller/tweet-controller";
+import multer from "multer";
 
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 export const privateApi =  express.Router();
 privateApi.use(authMiddleware);
+
 
 // User Routes
 privateApi.post("/api/v1/users", userController.createUserController);
@@ -12,7 +16,7 @@ privateApi.get("/api/v1/users", userController.getAllUsersController);
 privateApi.get("/api/v1/users/:id", userController.getUserByIDController);
 privateApi.put("/api/v1/users/:id", userController.updateUserController);
 privateApi.delete("/api/v1/users/:id", userController.deleteUserController);
-privateApi.post("/api/v1/upload/:id", userController.uploadAvatarController);
+privateApi.post("/api/v1/upload/:id", upload.single("avatar"), userController.uploadAvatarController);
 
 // Tweet Routes
 privateApi.post("/api/v1/tweets", tweetController.createTweetController);
